42615b74a85f23ca444e249aa8747f9662789b8b
yongyesun <68177406+yongyesun@users.noreply.github.com>
2022-11-10 18:58:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ proxy_list=[]
 
 if __name__ == '__main__':
   nodes = 'input.yaml'
-  with open(nodes, 'r') as reader:
+  with open(nodes, encoding = 'utf-8').read() as reader:
                 utf8_to_unicode =  reader.decode("utf-8")
                 reader = utf8_to_unicode.encode("utf-8")
                 working = yaml.load(reader, Loader=SafeLoader)
