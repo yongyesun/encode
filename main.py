a57242de683ee157e36ed6354d4d4c14c99e9d6e
yongyesun <68177406+yongyesun@users.noreply.github.com>
2022-11-10 18:47:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ proxy_list=[]
 if __name__ == '__main__':
   nodes = 'input.yaml'
   with open(nodes, 'r') as reader:
-    working = yaml.load(reader, Loader=SafeLoader)
-    for x in working['proxies']:
-      proxy_list.append(x)
-    proxies = makeclash(proxy_list)
-    push(proxies)
+                utf8_to_unicode =  reader.decode("utf-8")
+                reader = utf8_to_unicode.encode("utf-8")
+                working = yaml.load(reader, Loader=SafeLoader)
+                for x in working['proxies']:
+                        proxy_list.append(x)
+                proxies = makeclash(proxy_list)
+                push(proxies)
