92a48d23e89de203bc1b47168590e64ff48d9111
yongyesun <68177406+yongyesun@users.noreply.github.com>
2022-11-11 20:45:03 +0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -10,10 +10,9 @@ def makeclash(dictin):
                 if y['type'] in badprotocols:
                     pass
                 else:
-                    print("type",y['type'])
                     proxies.append(y)
                     proxies_noname.append(y[1:])
         except:
             continue
-    print("len",len(proxies))
+    print("len",len(proxies),"qichong",len(proxies_noname))
     return proxies
