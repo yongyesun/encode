94f9925cd05c3b3805e3be5cbc801d40b9e8ec6f
yongyesun <68177406+yongyesun@users.noreply.github.com>
2022-11-12 00:35:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,7 @@ proxy_list=[]
 
 if __name__ == '__main__':
   nodes = 'input.yaml'
-  with open(nodes, 'r') as reader:
-                """
-                reader_list = reader.readlines()                
-                for i in range(reader_list.index('proxies:\n')+1,reader_list.index('proxy-groups:\n')):
-                        reader_list[i] = reader_list[i].strip()
-                        reader_list[i] = reader_list[i].strip("-")
-                        reader_list[i] = reader_list[i].strip()
-                        reader_list[i] = reader_list[i].strip("{}")
-                        reader_list[i]
-                        proxy_list.append(reader_list[i])
-                """        
+  with open(nodes, 'r') as reader:       
                 working = yaml.load(reader, Loader=BaseLoader)
                 for x in working['proxies']:
                         proxy_list.append(x)
@@ -26,3 +16,7 @@ if __name__ == '__main__':
                 #proxy_list=list(proxy_list)
                 proxies = makeclash(proxy_list)
                 push(proxies)
+
+"""                
+   https://github.com/yu-steven/openit/
+"""   
