b3308f6518ee1ee710509fed9a18b3cf2257fd3f
yongyesun <68177406+yongyesun@users.noreply.github.com>
2022-11-11 17:51:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
-import yaml
-from yaml.loader import SafeLoader
+#import yaml
+#from yaml.loader import SafeLoader
 from parse import makeclash
 from clash import push
         
@@ -10,16 +10,13 @@ if __name__ == '__main__':
   nodes = 'input.yaml'
   with open(nodes, 'r') as reader:
                 reader_list = reader.readlines()
-                working = []
                 for i in range(reader_list.index('proxies:\n')+1,reader_list.index('proxy-groups:\n')):
                         reader_list[i] = reader_list[i].strip()
                         reader_list[i] = reader_list[i].strip("-")
                         reader_list[i] = reader_list[i].strip()
                         reader_list[i] = reader_list[i].strip("{}")
                         reader_list[i] = reader_list[i].split(",")
-                        working.append(reader_list[i])
-                print(working)
-                #for x in working['proxies']:
-                        #proxy_list.append(x)
-                #proxies = makeclash(proxy_list)
-                #push(proxies)
+                        proxy_list.append(reader_list[i])
+                #print(proxy_list)
+                proxies = makeclash(proxy_list)
+                push(proxies)
