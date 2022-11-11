98419c424699f16109e4d6a3a74a5ce4b2335cb8
yongyesun <68177406+yongyesun@users.noreply.github.com>
2022-11-11 17:07:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                 reader_list = reader.readlines()
                 working = []
                 for i in range(reader_list.index('proxies:\n'),reader_list.index('proxy-groups:\n')+1):
-                        working.append(reader_list(i))
+                        working.append(reader_list[i])
                 print(working)
                 #for x in working['proxies']:
                         #proxy_list.append(x)
