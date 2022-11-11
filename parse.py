ffa727e83e597b4dc4761635754d0698cbb9b9d4
yongyesun <68177406+yongyesun@users.noreply.github.com>
2022-11-11 18:08:32 +0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,15 +1,17 @@
 def makeclash(dictin):
     badprotocols = ['vless']
     proxies = []
+    proxies_noname = []
     for y in dictin:
         try:
-            if y in proxies:
+            if y in proxies_noname:
                 pass
             else:
                 if y['type'] in badprotocols:
                     pass
                 else:
                     proxies.append(y)
+                    proxies_noname.append(y[1:])
         except:
             continue
-    return 
+    return proxies
