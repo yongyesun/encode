553d8d49146610730d31df11733457683d702e55
yongyesun <68177406+yongyesun@users.noreply.github.com>
2022-12-01 00:27:09 +0800

    Update clash.py

--- clashcheck/clash.py
+++ clashcheck/clash.py
@@ -17,43 +17,41 @@ def push(list, outfile):
             {'name': 'automatic', 'type': 'url-test', 'proxies': [], 'url': 'https://www.google.com/favicon.ico',
              'interval': 300}, {'name': '🌐 Proxy', 'type': 'select', 'proxies': ['automatic']}],
              'rules': ['MATCH,🌐 Proxy']}
-#with maxminddb.open_database('Country.mmdb') as countrify:
-    for i in tqdm(range(int(len(list))), desc="Parse"):
-        x = list[i]
-        """
-        try:
-            float(x['password'])
-        except:
+    with maxminddb.open_database('Country.mmdb') as countrify:
+        for i in tqdm(range(int(len(list))), desc="Parse"):
+            x = list[i]
             try:
-                float(x['uuid'])
+                float(x['password'])
             except:
                 try:
-                    ip = str(socket.gethostbyname(x["server"]))
+                    float(x['uuid'])
                 except:
-                    ip = str(x["server"])
-                try:
-                    country = str(countrify.get(ip)['country']['iso_code'])
-                except:
-                    country = 'UN'
-                if country == 'TW' or country == 'MO' or country == 'HK':
-                    flagcountry = 'CN'
-                else:
-                    flagcountry = country
-                try:
-                    country_count[country] = country_count[country] + 1
-                    x['name'] = str(flag.flag(flagcountry)) + " " + country + " " + str(count)
-                except:
-                    country_count[country] = 1
-                    x['name'] = str(flag.flag(flagcountry)) + " " + country + " " + str(count)
-                clash['proxies'].append(x)
-                clash['proxy-groups'][0]['proxies'].append(x['name'])
-                clash['proxy-groups'][1]['proxies'].append(x['name'])
-                count = count + 1
-                """
-        clash['proxies'].append(x)
-        clash['proxy-groups'][0]['proxies'].append(x['name'])
-        clash['proxy-groups'][1]['proxies'].append(x['name'])
-        count = count + 1
+                    try:
+                        ip = str(socket.gethostbyname(x["server"]))
+                    except:
+                        ip = str(x["server"])
+                    try:
+                        country = str(countrify.get(ip)['country']['iso_code'])
+                    except:
+                        country = 'UN'
+                    """
+                    if country == 'TW' or country == 'MO' or country == 'HK':
+                        flagcountry = 'CN'
+                    else:
+                        flagcountry = country
+                    """
+                    try:
+                        country_count[country] = country_count[country] + 1
+                        #x['name'] = str(flag.flag(flagcountry)) + " " + country + " " + str(count)
+                        x['name'] = str(flag.flag(flagcountry)) + " " + country + " " + str(country_count[country])
+                    except:
+                        country_count[country] = 1
+                        #x['name'] = str(flag.flag(flagcountry)) + " " + country + " " + str(count)
+                        x['name'] = str(flag.flag(flagcountry)) + " " + country + " " + str(country_count[country])
+                    clash['proxies'].append(x)
+                    clash['proxy-groups'][0]['proxies'].append(x['name'])
+                    clash['proxy-groups'][1]['proxies'].append(x['name'])
+                    count = count + 1
 
     with open(outfile, 'w') as writer:
         yaml.dump(clash, writer, sort_keys=False)
