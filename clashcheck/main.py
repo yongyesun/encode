260fb6ab73d5a1fb42cf6dd59044cfcab6531891
yongyesun <68177406+yongyesun@users.noreply.github.com>
2022-12-01 12:34:24 +0800

    Update main.py

--- clashcheck/main.py
+++ clashcheck/main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         clashname, operating_system = checkenv()
         checkuse(clashname[2::], operating_system)
         clash = subprocess.Popen([clashname, '-f', './temp/working.yaml', '-d', '.'])
-        cmd("netstat -lnp")
+        subprocess.Popen("netstat -lnp",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8")
         processes =[]
         sema = Semaphore(threads)
         time.sleep(5)
