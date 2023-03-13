fff533d0e60ce2eda9646982f81c8706f67784b3
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-03-13 17:39:49 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -764,7 +764,6 @@ class sub_convert():
             return yaml_content
         except Exception as err:
             print(f'yaml decode 发生 {err} 错误')
-            print(repr(err))
             return '订阅内容解析错误'
 
     def base64_decode(url_content):  # Base64 转换为 URL 链接内容
