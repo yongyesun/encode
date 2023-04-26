01a70c08f8160a88e5ad3083cab7fd894dce2ad0
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-04-27 00:49:07 +0800

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -24,6 +24,6 @@ mv ./nodeencode99 ./subconverter
 mv ./nodeencode98 ./subconverter
 mv ./nodeencode97 ./subconverter
 mv ./nodeencode96 ./subconverter
-#echo "url=nodeencode100|nodeencode99|nodeencode98|nodeencode97|nodeencode96|nodeencode13|nodeencode14|nodeencode15|nodeencode16|nodeencode|nodeencode1" >> ./node_profile.ini
-echo "url=nodeencode16|nodeencode17" >> ./node_profile.ini
+echo "url=nodeencode100|nodeencode99|nodeencode98|nodeencode97|nodeencode96|nodeencode13|nodeencode14|nodeencode15nodeencode16|nodeencode17|nodeencode|nodeencode1" >> ./node_profile.ini
+#echo "url=nodeencode16|nodeencode17" >> ./node_profile.ini
 cat ./node_profile.ini
