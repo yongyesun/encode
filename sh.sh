cfdcd7a5085104829a04112a2a2dd40f31236b75
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-04-26 20:18:55 +0800

    Create sh.sh

---
+++ sh.sh
@@ -0,0 +1,11 @@
+         wget -q https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/Splitted/ss.txt -O ./nodeencode96
+         wget -q https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/Splitted/vless.txt -O ./nodeencode97
+         wget -q https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/Splitted/trojan.txt -O ./nodeencode98
+         wget -q https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/Splitted/ssr.txt -O ./nodeencode99
+         wget -q https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/Splitted/vmess.txt -O ./nodelist
+         base64 ./nodelist > nodeencode100 -w 0 || echo "It is a test"
+         mv ./nodeencode100 ./subconverter
+         mv ./nodeencode99 ./subconverter
+         mv ./nodeencode98 ./subconverter
+         mv ./nodeencode97 ./subconverter
+         mv ./nodeencode96 ./subconverter
